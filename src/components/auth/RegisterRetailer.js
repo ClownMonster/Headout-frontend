b28b17415813e3fbc048c96f2e65fb697e279ba6
Manvendra Bansal <bansalmanvendra@gmail.com>
2020-11-07 15:03:07 +0530

    register state done for retailer

--- src/components/auth/RegisterRetailer.js
+++ src/components/auth/RegisterRetailer.js
@@ -6,7 +6,22 @@ import "./register.css"
 
 const RegisterRetailer = () => {
 
+    const [registerData,setRegisterData] = useState({
+      vendorName:"",
+      vendorId: null,
+      vendorGovRegsNumber: "",
+      vendorPassword:"",
+      vendorAddress:"",
+      vendorEmail:"",
+      vendorNumber: ""
+    }) 
+
+    // const [signInData,setSignIndata] = useState({
+      
+    // })
+
     const [form,setForm] = useState(false);
+    
     let divClass = ["container1"];
     if(form) {
       divClass.push('sign-up-mode');
@@ -18,16 +33,15 @@ const RegisterRetailer = () => {
    const signIn = () => { 
      setForm(false)
     }
+    const { vendorName, vendorEmail, vendorPassword, vendorGovRegsNumber,vendorNumber } = registerData;
+    const onRegister = e => setRegisterData({ ...registerData, [e.target.name]: e.target.value })
+    // const onSingIn = e => setSignInData({ ...formData, [e.target.name]: e.target.value })
 
-
-
-
-
-
+    const onRegisterSubmit = e =>{
+      e.preventDefault()
+      console.log(registerData)
+    }
      
-
-
-
 
   return (
     <div className={divClass.join(" ")}>
@@ -46,25 +60,29 @@ const RegisterRetailer = () => {
             <input type="submit" value="Login" className="btn solid" />
           </form>
 
-          <form action="#" className="sign-up-form">
+          <form action="#" onSubmit={e => onRegisterSubmit(e)} className="sign-up-form">
             <h2 className="title">Sign up</h2>
             <div className="input-field">
               <i className="fas fa-user"></i>
-              <input type="text" placeholder="Vendor Name" />
+              <input value={vendorName} onChange={e => onRegister(e)} name ="vendorName" type="text" placeholder="Vendor Name" />
             </div>
             <div className="input-field">
               <i className="fas fa-user"></i>
-              <input type="text" placeholder="Govt Reg No" />
+              <input value={vendorGovRegsNumber} onChange={e => onRegister(e)} name ="vendorGovRegsNumber" type="text" placeholder="Govt Reg No" />
             </div>
             <div className="input-field">
               <i className="fas fa-envelope"></i>
-              <input type="email" placeholder="Email" />
+              <input value={vendorEmail} onChange={e => onRegister(e)} name ="vendorEmail" type="email" placeholder="Email" />
             </div>
             <div className="input-field">
               <i className="fas fa-lock"></i>
-              <input type="password" placeholder="Password" />
+              <input value={vendorPassword} onChange={e => onRegister(e)} name ="vendorPassword" type="password" placeholder="Password" />
             </div>
-            <input type="submit" className="btn" value="Sign up" />
+            <div className="input-field">
+              <i className="fas fa-user"></i>
+              <input value={vendorNumber} onChange={e => onRegister(e)} name ="vendorNumber" type="text" placeholder="Mobile Number" />
+            </div>
+            <input  type="submit" className="btn" value="Sign up" />
           </form>
         </div>
       </div>

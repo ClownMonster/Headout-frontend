c4fa4ec604391d369a406d6659e2d0d9b1866605
ClownMonster <mk6386223@gmail.com>
2020-11-07 17:42:51 +0530

    sinup change

--- src/components/orders/OrderDetail.js
+++ src/components/orders/OrderDetail.js
@@ -1,27 +1,9 @@
-import React, {useEffect,useState} from "react";
+import React from "react";
 import {Link} from 'react-router-dom';
-
 import './vorder.css'
 import vlogo from './vcart.svg'
 
-
-const VendorOder = ()=>{
-
-  const {order,setOrder}  = useState ([])
-
-  useEffect( ()=>{
-    fetch('http://hackout.herokuapp.com/getAllOrders?vendorId=2334333',{
-    }).then(
-      res => res.json()
-    ).then(data => setOrder(data) )
-    .catch(
-      err=> console.error(err)
-    )
-    },[] )
-
-
-
-   
+const OrderDetail = ()=>{
   return (
     <div className="outer">
     <div className="leftBar">
@@ -45,11 +27,10 @@ const VendorOder = ()=>{
     </div>
     <div className="mainArea">
         <div className="vlogo"><img src={vlogo}  alt="vlogo" /></div>
-
     </div>
     </div>
   );
 
 }
 
-export default VendorOder;
+export default OrderDetail;
